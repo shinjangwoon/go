07b08b9aa849c297d4f14b25809faab88c975835
shinjangwoon <83771596+shinjangwoon@users.noreply.github.com>
2023-12-15 17:11:11 +0900

    change db

--- src/go_study/database/database.go
+++ src/go_study/database/database.go
@@ -12,7 +12,7 @@ import (
 var DBConn *gorm.DB
 
 func ConnectDB() {
-	dsn := "root:tlswkddns89^^@tcp(127.0.0.1:3306)/fiber_blog?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:woon@tcp(127.0.0.1:3306)/fiber_blog?charset=utf8mb4&parseTime=True&loc=Local"
 
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
 		Logger: logger.Default.LogMode(logger.Error),
