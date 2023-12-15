d3a2bdeb102d02cdf109a43bb5e7c003a2575229
shinjangwoon <83771596+shinjangwoon@users.noreply.github.com>
2023-12-15 17:10:21 +0900

    first_golang

---
+++ src/go_study/example/variables.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a string = "initial"
+	fmt.Println(a)
+
+	var b, c int = 1, 2
+	fmt.Println(b, c)
+	fmt.Print(b, c)
+
+	var d = true
+	fmt.Println(d)
+	fmt.Print(d)
+
+	var e int
+	fmt.Println(e)
+
+	f := "short"
+	fmt.Println(f)
+
+}
---
+++ src/go_study/server.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"example/hello/database"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func init() {
+	database.ConnectDB()
+}
+
+func main() {
+
+	database.ConnectDB()
+
+	sqlDb, err := database.DBConn.DB()
+
+	if err != nil {
+		panic("Error in sql connection: ")
+	}
+
+	defer sqlDb.Close()
+
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+
+		return c.JSON(fiber.Map{"message": "Welcome to Myfirst Web Application"})
+
+	})
+
+	app.Listen(":8000")
+}
