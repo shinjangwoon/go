19407b7ca113c2551b9e6a0e9ce6cad585e22ae4
shinjangwoon <83771596+shinjangwoon@users.noreply.github.com>
2023-12-23 17:55:19 +0900

    React

---
+++ go_fiber/client/src/App.js
@@ -0,0 +1,15 @@
+import { Container, Row, Col } from "react-bootstrap"
+
+function App() {
+  return (
+    <Container>
+      <Row>
+        <Col xs="12 py-2">
+          <h1 className="text-center ">React Application with Go fiber Backend</h1>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default App;
